e0995ff321829f6f97a4d64bfbc2042fd8f2dee6
raquelgmn <raquelgarciamonsalve@gmail.com>
2022-09-28 19:14:09 +0200

    Initial commit

---
+++ src/main/java/com/raquel/cantaloupe/CantaloupeApplication.java
@@ -0,0 +1,13 @@
+package com.raquel.cantaloupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantaloupeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantaloupeApplication.class, args);
+	}
+
+}
